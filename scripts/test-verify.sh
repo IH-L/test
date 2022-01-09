1d12ea7aa9da4265d7a360e3924d1bc832b8a643
IIIIIH <50024173+IH-L@users.noreply.github.com>
2022-01-09 16:23:05 +0800

    Update test-verify.sh

--- scripts/test-verify.sh
+++ scripts/test-verify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ `diff animal.txt data/animal.txt | wc -l` -ne 0 ]; then
+if [ `diff animal.txt data/animal-data.txt | wc -l` -ne 0 ]; then
 	echo "animal.txt content is incorrect!"
 	exit 1
 fi
